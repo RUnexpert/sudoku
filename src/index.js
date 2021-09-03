34c148431300b2004f6444c2a5aba68458f40433
RUnexpert <aldali911@gmail.com>
2021-09-03 15:18:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
 module.exports = function solveSudoku(matrix) {
-  let nonPossibilities = {};
-  let impossibleNumbers;
-  let emptySpaces = 81;
-  while (emptySpaces > 0) {
-    emptySpaces = 0;
-    for (let vert = 0; vert < matrix.length; vert++) {
-      for (let horz = 0; horz < matrix.length; horz++) {
-        if (matrix[vert][horz] === 0) {
-          nonPossibilities = {};
-          for (let i = 0; i < 9; i++) {
-            if (matrix[vert][i] > 0) {
-              nonPossibilities[matrix[vert][i]] = true;
-            }
-            if (matrix[i][horz] > 0) {
-              nonPossibilities[matrix[i][horz]] = true;
-            }
-          }
-          for (let vertBox = Math.floor(vert / 3) * 3; vertBox < Math.floor(vert / 3) * 3 + 3; vertBox++) {
-            for (let horzBox = Math.floor(horz / 3) * 3; horzBox < Math.floor(horz / 3) * 3 + 3; horzBox++) {
-              if (matrix[vertBox][horzBox]) {
-                nonPossibilities[matrix[vertBox][horzBox]] = true;
+  solve();
+  return matrix;
+  
+  function solve() {
+      for(let row = 0; row < 9; ++row) {
+          for(let col = 0; col < 9; ++col) {
+              if(matrix[row][col] !== 0) {
+                  continue;
               }
-            }
-          }
-          impossibleNumbers = Object.keys(nonPossibilities);
-          if (impossibleNumbers.length === 8) {
-            for (let i = 1; i < 10; i++) {
-              if (impossibleNumbers.indexOf(String(i)) < 0) {
-                matrix[vert][horz] = i;
+              for(let v = 1; v <= 9; ++v) {
+                  const v_s = v;
+                  if(isValid(row, col, v_s)) {
+                    matrix[row][col] = v_s;
+                      if(solve()) {
+                          return true;
+                      } else {
+                        matrix[row][col] = 0;
+                      }
+                  }
               }
-            }
-          } else {
-            emptySpaces++;
+              return false;
           }
-        }
       }
-    }
+      return true;
   }
-return matrix;
+  
+  function isValid(row, col, v) {
+      const regionRow = 3 * Math.floor(row / 3);    
+      const regionCol = 3 * Math.floor(col / 3);    
+      for (let i = 0; i < 9; i++) {
+          if (matrix[i][col] === v) return false;    
+          if (matrix[row][i] === v) return false;   
+          const cube_r = regionRow + Math.floor(i / 3),
+                cube_c = regionCol + i % 3;
+          if (matrix[cube_r][cube_c] === v) return false;
+      }
+      return true;
+  }
 }
